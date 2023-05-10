8d2677f4340bdaa1eee246c819533ce87124e9db
Jsheen98 <jsheendev@gmail.com>
2023-05-09 18:48:04 -0600

    Added fixes to saved and search mutations/queries and related files

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -21,6 +21,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -73,12 +74,9 @@ const SearchBooks = () => {
     }
 
     try {
-      // const response = await saveBook(bookToSave, token); //remove?
-      const [saveBook, { error, data }] = useMutation(SAVE_BOOK)
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { input: { ...bookToSave } },
+      })
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,6 +20,15 @@ type Book {
     link: String
 }
 
+input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
 type Auth {
     token: ID!
     user: User
@@ -32,7 +41,7 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [String], description: String, title: String, bookId: ID!, image: String, link: String): User
+    saveBook(input: BookInput!): User
     removeBook(bookId: ID!): User
 }
 `
